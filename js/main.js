e7d336196b6c6ab187aa99393db033b729b1ab4f
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-08-10 19:16:48 -0300

    Adicionado estilização da pagina e no resultado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-window.onload = () => {
-    "use strict";
-    if("serviceWorker" in navigator){
-        navigator.serviceWorker.register("./sw.js");
-    }
-};
+// window.onload = () => {
+//     "use strict";
+//     if("serviceWorker" in navigator){
+//         navigator.serviceWorker.register("./sw.js");
+//     }
+// };
+
+function calculo(){
+    let altura = document.getElementById('altura').value;
+    let largura = document.getElementById('largura').value;
+    let calculo = "O resultado da área é " + Number(altura * largura);
+    document.getElementById('result').textContent = calculo;
+}
