0e8e700095aa672e729b046c2616e3b45341b467
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-08-17 16:47:42 -0300

    pwa ativado

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// window.onload = () => {
-//     "use strict";
-//     if("serviceWorker" in navigator){
-//         navigator.serviceWorker.register("./sw.js");
-//     }
-// };
+window.onload = () => {
+    "use strict";
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register("./sw.js");
+    }
+};
 
 function calculo(){
     let altura = document.getElementById('altura').value;
